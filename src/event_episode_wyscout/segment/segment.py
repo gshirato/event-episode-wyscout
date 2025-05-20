8f69535eb97dcbd3b1ad1719ca4e27d5e85de74b
gshirato <g8ta18@gmail.com>
2025-05-20 19:24:11 +0200

    feat(segment): enhance event segmentation logic to include 'infraction' and 'loose_ball_duel'

--- src/event_episode_wyscout/segment/segment.py
+++ src/event_episode_wyscout/segment/segment.py
@@ -11,18 +11,25 @@ def clear_start(primary: str, secondary: str) -> bool:
 
 
 def clear_end(primary: str, secondary: str) -> bool:
-    if primary in ["game_interruption", "offside"]:
+    if primary in ["game_interruption", "offside", "infraction"]:
         return True
     if "ball_out" in secondary:
         return True
     return False
 
 
-def segment_events(event: dict) -> tuple[list[tuple], list[tuple]]:
+def loose_ball(secondary: str) -> bool:
+    if "loose_ball_duel" in secondary:
+        return True
+    return False
+
+
+def segment_events(event: dict) -> tuple[list[tuple], list[tuple], list[tuple]]:
     start = []
     end = []
+    loose = []
 
-    for ev in event:
+    for i, ev in enumerate(event):
         id = ev["id"]
         team = ev["team"]["id"]
         primary = ev["type"]["primary"]
@@ -34,5 +41,10 @@ def segment_events(event: dict) -> tuple[list[tuple], list[tuple]]:
             start.append(id)
         if clear_end(primary, secondary):
             end.append(id)
+        if loose_ball(secondary):
+            loose.append(id)
 
-    return start, end
+    return start, end, loose
+
+
+def group_
